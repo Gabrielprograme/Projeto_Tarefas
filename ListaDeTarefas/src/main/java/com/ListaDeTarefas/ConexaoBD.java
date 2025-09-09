95a30cb9b4944d822c95cbf7a4d510a265f69ca6
Arthur <arthurbruno489@gmail.com>
2025-09-09 20:08:46 -0300

    Update ConexaoBD.java

--- ListaDeTarefas/src/main/java/com/ListaDeTarefas/ConexaoBD.java
+++ ListaDeTarefas/src/main/java/com/ListaDeTarefas/ConexaoBD.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection
+public class ConexaoDB
 {
     private static final Dotenv env = Dotenv.load();
     private static final String URL = env.get("SUPABASE_URL");
